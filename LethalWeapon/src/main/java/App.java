eb3d7f660ea2db78f478d4924337dce5edecc57d
Paul Watson <pwatson@Zips-MacBook-Pro.local>
2015-10-17 17:22:47 -0400

    Saturday Oct. 17, commit

--- LethalWeapon/src/main/java/App.java
+++ LethalWeapon/src/main/java/App.java
@@ -1,10 +1,58 @@
-
+import java.util.Scanner;
 /**
  * Created by pwatson on 10/16/15.
  */
 public class App {
+    int score = 30;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public void personInput() {
+        Person person = new Person();
+        ConsumerData consumerData = new ConsumerData();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your name.");
+        person.setName(scanner.nextLine());
+        System.out.println("Please enter Date of birth in date format.");
+        person.setDOB(scanner.nextLine());
+        System.out.println("Are you married? Please enter y or n for yes or no.");
+        person.setMarital (scanner.nextLine());
+        System.out.println("Please enter your address.");
+        person.setAddress(scanner.nextLine());
+        System.out.println("Please enter the length of your current residence.");
+        String residence = scanner.nextLine();
+        consumerData.setLor(Integer.parseInt(residence));
+        System.out.println("Please enter your rent per month.");
+        consumerData.setRentBuyOwn(scanner.nextLine());
+        System.out.println("Please enter the length of you current employment (in years).");
+        consumerData.setEmployed(scanner.nextLine());
+        int loe = Integer.parseInt(employment);
+        System.out.println("Please enter your annual household income.");
+        String income = scanner.nextLine();
+        double tHI = Double.parseDouble(income);
+        System.out.println("Please enter your FICO score.");
+        String fcoscr = scanner.nextLine();
+        int fico = Integer.parseInt(fcoscr);
+        System.out.println("Please enter the number of months you have had outstanding credit.");
+        String fcyrs = scanner.nextLine();
+        int ficoYears = Integer.parseInt(fcyrs);
+        System.out.println("Please enter your total household debt.");
+        String expense = scanner.nextLine();
+        double tHD = Double.parseDouble(expense);
+        System.out.println("Please enter your amount of collateral");
+        String col = scanner.nextLine();
+        double collateral = Double.parseDouble(col);
+        System.out.println("Please enter your amount of capital");
+        String cap = scanner.nextLine();
+        double capital = Double.parseDouble(cap);
+    }
     public static void main(String[] args) {
-        if (score > 20){
+        Person person = new Person();
+        ConsumerData consumerData = new ConsumerData();
+        App app = new App();
+        app.personInput();
+
+
+        if (app.score > 20) {
             System.out.println("Your loan has been approved! Congratulations!");
         }
     }
--- LethalWeapon/src/main/java/ConsumerData.java
+++ LethalWeapon/src/main/java/ConsumerData.java
@@ -23,6 +23,75 @@ public class ConsumerData {
     int ficoYears = 0;
     int score = 30;
 
+
+    public void setEmployed(boolean employed) {
+        this.employed = employed;
+    }
+
+    public void setSocial(String social) {
+        this.social = social;
+    }
+
+    public void setLor(int lor) {
+        this.lor = lor;
+    }
+
+    public void setLoe(int loe) {
+        this.loe = loe;
+    }
+
+    public void setRentBuyOwn(char rentBuyOwn) {
+        this.rentBuyOwn = rentBuyOwn;
+    }
+
+    public void setHousingExpense(double housingExpense) {
+        this.housingExpense = housingExpense;
+    }
+
+    public void settHI(double tHI) {
+        this.tHI = tHI;
+    }
+
+    public void settHD(double tHD) {
+        this.tHD = tHD;
+    }
+
+    public void setMhi(double mhi) {
+        this.mhi = mhi;
+    }
+
+    public void setMdi(double mdi) {
+        this.mdi = mdi;
+    }
+
+    public void setDisposabe(double disposabe) {
+        this.disposabe = disposabe;
+    }
+
+    public void setCollateral(double collateral) {
+        this.collateral = collateral;
+    }
+
+    public void setCapital(double capital) {
+        this.capital = capital;
+    }
+
+    public void setLoanAmt(double loanAmt) {
+        this.loanAmt = loanAmt;
+    }
+
+    public void setFico(int fico) {
+        this.fico = fico;
+    }
+
+    public void setFicoYears(int ficoYears) {
+        this.ficoYears = ficoYears;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
     private static Scanner scanner = new Scanner(System.in);
 
     public void ability() {
