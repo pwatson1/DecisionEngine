90b922c705a06318076af5a4c73237ef71091f2d
Gregory Suhocki <gsuhocki@Zips-MacBook-Pro-3.local>
2015-10-17 17:22:30 -0400

    First Commit SuhockiDecisionEngine

--- LethalWeapon/src/main/java/ConsumerData.java
+++ LethalWeapon/src/main/java/ConsumerData.java
@@ -1,38 +1,107 @@
-import java.util.Scanner;
-
 /**
  * Created by pwatson on 10/16/15.
  */
 public class ConsumerData {
 
-    boolean employed = true;
     String social;
-    int lor = 0;
-    int loe = 0;
-    char rentBuyOwn;
-    double housingExpense = 0;
+    String residence;
+    String lengthofEm;
+    String rentBuyOwn;
     double tHI = 0;
-    double tHD = 0;
-    double mhi = 0;
-    double mdi = 0;
-    double disposabe = 0;
-    double collateral = 0;
-    double capital = 0;
-    double loanAmt = 0;
-    int fico = 0;
-    int ficoYears = 0;
+    String houseInc;
+    String houseDis;
+    String monthlyInc;
+    String monthlyDis;
+    double disposable = 0;
+    String col;
+    String cap;
+    String loan;
+    String ficScore;
+    String ficoYrs;
     int score = 30;
+    String employed;
 
-    private static Scanner scanner = new Scanner(System.in);
+    public void setEmployed(String employed) {
+        this.employed = employed;
+    }
+
+    public void setSocial(String social) {
+        this.social = social;
+    }
+
+    public void setLor(String residence) {
+        this.residence = residence;
+    }
+
+    public void setLoe(String lengthofEm) {
+        this.lengthofEm = lengthofEm;
+    }
+
+    public void setRentBuyOwn(String rentBuyOwn) {
+        this.rentBuyOwn = rentBuyOwn;
+    }
+
+    public void settHI(String houseInc) {
+        this.houseInc = houseInc;
+    }
+
+    public void settHD(String houseDis) {
+        this.houseDis = houseDis;
+    }
+
+    public void setMhi(String monthlyInc) {
+        this.monthlyInc = monthlyInc;
+    }
+
+    public void setMdi(String monthlyDis) {
+        this.monthlyDis = monthlyDis;
+    }
+
+    public void setDisposable(String disposable) {
+        this.disposable = Double.parseDouble(disposable);
+    }
+
+    public void setCollateral(String col) {
+        this.col = col;
+    }
+
+    public void setCapital(String cap) {
+        this.cap = cap;
+    }
+
+    public void setLoanAmt(String loan) {
+        this.loan = loan;
+    }
+
+    public void setFico(String ficScore) {
+        this.ficScore = ficScore;
+    }
+
+
+    public void setFicoYears(String ficoYrs) {
+        this.ficoYrs = ficoYrs;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
 
     public void ability() {
-        if(employed == true) {
+
+        //Must be declared where they are actioned
+        Double mhi = Double.parseDouble(monthlyInc);
+        Integer collateral = Integer.parseInt(col);
+        Integer capital = Integer.parseInt(cap);
+        Double loanAmt = Double.parseDouble(loan);
+
+        if(employed.equals("Y")) {
             score = score - 3;
         }
         if(tHI < 20800 ) {
             score = score -2;
         }
-        if((disposabe/mhi) < .15) {
+        if((disposable /mhi) < .15) {
             score = score -3;
         }
         if(!(collateral/loanAmt >= 3)) {
@@ -45,15 +114,20 @@ public class ConsumerData {
 
     public void stability() {
 
-    if(lor < 3) {
-        score = score = -1;
-    }else if(lor < 2) {
-        score = score - 2;
-    }else{
-        score = score - 3;
-    }
+        //Must be declared where actioned
+        Integer lor = Integer.parseInt(residence);
+        Integer loe = Integer.parseInt(lengthofEm);
+        Integer ficoYears = Integer.parseInt(ficoYrs);
+
+        if(lor < 3) {
+            score = score -1;
+        }else if(lor < 2) {
+            score = score - 2;
+        }else{
+            score = score - 3;
+        }
         if(loe < 3) {
-            score = score = -1;
+            score = score -1;
         }else if(loe < 2) {
             score = score - 2;
         }else{
@@ -70,7 +144,11 @@ public class ConsumerData {
             score = score - 4;
         }
     }
-    public void WillingNessToPay(){
+    public void willingNessToPay(){
+
+        //Must be declared where actioned
+        Integer fico = Integer.parseInt(ficScore);
+
         if(fico>690){
             score -=2;
         }else if(fico<=690 && fico>650){
@@ -81,13 +159,21 @@ public class ConsumerData {
             score -=10;
         }
 
-       }
+    }
 
     public void disposable() {
+
+        //Call where actioned
+        Double tHD = Double.parseDouble(houseDis);
+
+        //Declare this type of variable
+        Double mdi;
+        Double mhi;
+
         mhi=(((tHI/52)*4)*.64);
         mdi=((tHD/52)*4);
-         disposabe = (mhi-mdi)/mhi;
-            }
-        }
+        disposable = (mhi-mdi)/mhi;
+    }
 
+}
 
